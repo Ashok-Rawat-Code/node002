ca2b601c1f1495bb274854cb3588f144e15bbd9a
Ashok Kumar Rawat <ashok.rawat.blr@gmail.com>
2023-11-06 20:22:42 +0530

    initial

---
+++ config.js
@@ -0,0 +1,91 @@
+var config = {}
+
+config.endpoint = 'https://akr.documents.azure.com:443/'
+config.key = '0ZvsvJP8SQOvl5cQ7nH2YAiymK2EHes9bjwOFbgcLKId9vD9eSw6SKhTK06Lp4ijRTLj3qsVdb6eACDbDDLRuw=='
+
+config.database = {
+  id: 'ToDoList'
+}
+
+config.container = {
+  id: 'Ashok'
+}
+
+config.items = {
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    partitionKey: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'Wakefield.7',
+    partitionKey: 'Italy',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config
